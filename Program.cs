ae3a40d8943cd603e29effc1d648be134099aebb
repoleved08 <geekbii08@gmail.com>
2023-08-09 02:41:48 +0300

    C# projects

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(String[] args){
+            System.Console.WriteLine("Hello world!");
+        }
+    }
+}
